78b9e4ea5b4652b90707665ad13b84783be2c84f
Alejandro Gutierrez <ajgutierrez@uc.cl>
2017-05-10 18:17:14 -0300

    auth not working

--- Application/config/routes.rb
+++ Application/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
+  devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   resources :series
   resources :capitulos
   get 'home/index'
 
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
