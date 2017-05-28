2d3bd39b971772df96403d9236ebaf71483b665d
fringlesinthestreet <bicarcamo@uc.cl>
2017-05-28 19:08:06 -0400

    perfiles

--- TvShowApp/app/helpers/user_helper.rb
+++ TvShowApp/app/helpers/user_helper.rb
@@ -1,12 +1,12 @@
 module UserHelper
 
-  def full_name (user)
-    if not user.first_name.nil? and not user.last_name.nil?
-      user.first_name + " " + user.last_name
-    else
-      "None"
-    end
-  end
+  #def full_name (user)
+  #  if not user.first_name.nil? and not user.last_name.nil?
+  #    user.first_name + " " + user.last_name
+  #  else
+  #    "None"
+  #  end
+  #end
 
   #def age (user)
   #  if not user.birthday.nil?
