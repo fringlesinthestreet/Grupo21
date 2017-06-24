98822d5a713b5571d7de8c18b9372cc27e189702
selira <selira@uc.cl>
2017-06-24 14:37:17 -0400

    seeds finalfinal

--- TvShowApp/db/seeds.rb
+++ TvShowApp/db/seeds.rb
@@ -12,20 +12,64 @@ user.id = 1
 user.name = 'admin'
 user.password = '123456'
 user.password_confirmation = '123456'
-user.email = 'admin@admin.com'
+user.email = 'admin@tvshows.com'
 user.admin = true
+user.save!
+
+user = User.new
+user.id = 2
+user.name = 'benja'
+user.password = '123456'
+user.password_confirmation = '123456'
+user.email = 'benja@tvshows.com'
+user.admin = false
+user.save!
+
+user = User.new
+user.id = 3
+user.name = 'ale'
+user.password = '123456'
+user.password_confirmation = '123456'
+user.email = 'ale@tvshows.com'
+user.admin = false
+user.save!
+
+user = User.new
+user.id = 4
+user.name = 'santi'
+user.password = '123456'
+user.password_confirmation = '123456'
+user.email = 'santi@tvshows.com'
+user.admin = false
 user.save!
 
 for i in 0..15
   show = TvShow.new
-  show.name = Faker::Book.title,
+  show.name =Faker::Book.title
   #por alguna razon el nombre queda como nil...
-  show.genre = Faker::Book.genre,
-  show.country = Faker::Address.country,
-  show.language = Faker::Lorem.word,
-  show.actors = Faker::Name.name,
-  show.director = Faker::Name.name,
-  show.seasons = Faker::Number.between(1,15),
-  show.user_id = 1,
+  show.genre = Faker::Book.genre
+  show.country = Faker::Address.country
+  show.language = Faker::Lorem.word
+  show.actors = Faker::Name.name
+  show.director = Faker::Name.name
+  show.seasons = Faker::Number.between(1,15)
+  show.user_id = 1
   show.save!
+end
+
+for i in 1..3
+  for j in 2..4
+
+    show = TvShow.new
+    show.name =Faker::Book.title
+    #por alguna razon el nombre queda como nil...
+    show.genre = Faker::Book.genre
+    show.country = Faker::Address.country
+    show.language = Faker::Lorem.word
+    show.actors = Faker::Name.name
+    show.director = Faker::Name.name
+    show.seasons = Faker::Number.between(1,15)
+    show.user_id = j
+    show.save!
+  end
 end
