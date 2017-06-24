83072ea5c7a0fce0660f3aa869134c7fb6bea5e7
selira <selira@uc.cl>
2017-06-24 13:34:04 -0400

    seeds

--- TvShowApp/db/seeds.rb
+++ TvShowApp/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.new
+user.id = 1
+user.name = 'admin'
+user.password = '123456'
+user.email = 'admin@admin.com'
+user.encrypted_password = '123456'
+user.admin = true
+user.save!
+
+for i in 0..15
+  show = TvShow.new
+  show.name = Faker::Book.title,
+  #por alguna razon el nombre queda como nil...
+  show.genre = Faker::Book.genre,
+  show.country = Faker::Address.country,
+  show.language = Faker::Lorem.word,
+  show.actors = Faker::Name.name,
+  show.director = Faker::Name.name,
+  show.seasons = Faker::Number.between(1,15),
+  show.user_id = 1,
+  show.save!
+end
